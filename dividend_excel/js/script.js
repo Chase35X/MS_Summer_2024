adeda06865a0d0aead87b5771420b76623a804a8
Chase35X <89099136+Chase35X@users.noreply.github.com>
2024-06-10 09:57:28 -0400

    uncomment download

--- dividend_excel/js/script.js
+++ dividend_excel/js/script.js
@@ -206,17 +206,17 @@ async function APIcall(tickers){
             throw new Error('Incorrect file type received')
         }
         
-        // const downloadURL = window.URL.createObjectURL(blob)
-        // const a = document.createElement('a')
-        // a.style.display = 'none'
-        // a.href = downloadURL
-        // a.download = 'workbook.xlsx'
+        const downloadURL = window.URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.style.display = 'none'
+        a.href = downloadURL
+        a.download = 'workbook.xlsx'
 
-        // document.body.appendChild(a)
-        // a.click()
+        document.body.appendChild(a)
+        a.click()
 
-        // document.body.removeChild(a)
-        // window.URL.revokeObjectURL(downloadURL)
+        document.body.removeChild(a)
+        window.URL.revokeObjectURL(downloadURL)
     })
 
     return response
